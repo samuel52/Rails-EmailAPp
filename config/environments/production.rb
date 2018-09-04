d500e1e87a6ebf627839a71886bb0dfe677cca6c
samuel52 <samuelsadiq52@gmail.com>
2018-09-04 23:02:15 +0000

    second

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # Setup the mailer config
-  # config.action_mailer.default_url_options = { host: 'https://still-garden-64282.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://still-garden-64282.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
